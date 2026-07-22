b60e072854783b7e8e9aff31539e859c17844382
Alishan Nadeem <alishan.nadeem22@gmail.com>
2026-07-23 03:33:33 +0500

    Refactor useReferralsController to utilize getReferredUsers query for total referrals and loading state management.

--- src/screens/Referrals/useReferralsController.js
+++ src/screens/Referrals/useReferralsController.js
@@ -1,13 +1,15 @@
 import { useCallback } from "react"
 import { Share } from "react-native"
 import { APP_NAME } from "../../config/env"
-import { REFERRAL_USERS } from "../../helpers/data"
 import { navigate } from "../../helpers/navigation"
 import { ROUTES } from "../../helpers/routes"
+import { useGetReferredUsersQuery } from "../../redux/apis/Referral"
 
 const REFERRAL_LINK = "https://jamatconnect.com/invite/lewis-hilton"
 
 const useReferralsController = () => {
+
+    const { data, isLoading } = useGetReferredUsersQuery()
 
     const onShare = useCallback(async () => {
         await Share.share({
@@ -22,7 +24,8 @@ const useReferralsController = () => {
 
     return {
         values: {
-            total_referrals: REFERRAL_USERS.length,
+            total_referrals: data?.pagination?.total ?? 0,
+            is_loading: isLoading,
         },
         functions: {
             onShare,
