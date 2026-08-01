dee1f311bc9cf3e5621ef186bf19f57c66fe16b8
Alishan Nadeem <alishan.nadeem22@gmail.com>
2026-08-01 22:33:23 +0500

    Add CategoryCard component: create a new reusable card component for displaying category information, including an image and name, with touchable functionality for user interaction.

---
+++ src/components/CategoryCard/index.jsx
@@ -0,0 +1,37 @@
+import { memo } from "react"
+import { StyleSheet } from "react-native"
+import colors from "../../helpers/colors"
+import { heightPixel } from "../../helpers/metrics"
+import Icon from "../Icon"
+import Text from "../Text"
+import Touchable from "../Touchable"
+
+const CategoryCard = ({ data, onPress }) => {
+
+    const { image_url, name } = data
+
+    return (
+        <Touchable onPress={onPress} style={styles.container}>
+            <Icon
+                source={{ uri: image_url }}
+                size={88}
+                rounded={"full"}
+                background={colors.white}
+                space
+            />
+            <Text size={11} weight="semibold" align="center" lines={2}>
+                {name}
+            </Text>
+        </Touchable>
+    )
+}
+
+export default memo(CategoryCard)
+
+const styles = StyleSheet.create({
+    container: {
+        width: heightPixel(88),
+        alignItems: "center",
+        gap: heightPixel(8),
+    }
+})
