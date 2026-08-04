1abcc624204ecf68683bea7e25b58ab3b001f3f3
Alishan Nadeem <alishan.nadeem22@gmail.com>
2026-08-04 23:14:37 +0500

    Add image placeholder: include a new image file for use in the application and update the images index to reference it, enhancing visual consistency.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,7 @@ const images = {
     full_logo: require("./full_logo.png"),
     logo: require("./logo.png"),
     avatar_one: require("./avatar_one.jpg"),
+    image_placeholder: require("./image-placeholder.jpg"),
 }
 
 export default images
